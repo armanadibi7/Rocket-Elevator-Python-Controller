4ba3a987d9333050faa0885c3b5bc00bc8d137a9
Arman Adibi <armanadibi7@gmail.com>
2021-10-15 18:14:57 -0400

    Final Version

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-# Rocket-Elevators-Python-Controller
-This is the template to use for the python residential controller. You will find the classes that should be used along with some methods described in the requirements. The necessary file to run some tests is also included. 
+# Rocket-Elevators-PYTHON-Controller
+This is a source code to Rocket Elevators Controller written in PYTHON by Arman Adibi.
 
-### Installation
+The test files make sure that the Controller are working correctly and it is safe
+to be implented into our customers building.
+
+Installation
 
 First, depending on your python version, make sure to install the Package Installer for Python (PIP) if needed:
 
@@ -11,24 +14,55 @@ Next, install Pytest:
 
 https://docs.pytest.org/en/6.2.x/getting-started.html
 
-### Running the tests
+WHEN ALL THE REQURIMENTS ARE THERE:
 
-To launch the tests:
+    TO RUN THE TEST , TYPE IN YOUR CONSOLE:
+        pytest
+    You can also get more details about each test by adding the -v flag:
 
-`pytest`
+    pytest -v
 
-With a fully completed project, you should get an output like:
+============== CONTENTS OF THIS PYTHON SOURCE CODE ============== 
+1. Column
 
-![Screenshot from 2021-06-15 13-13-13](https://user-images.githubusercontent.com/28630658/122095645-a41fa000-cddb-11eb-9322-81a766cce4bb.png)
+        * DECLARE ITS ATTRIBUTES (CONSTRUCTORS)
+        * CREATE ELEVATORS OBJECTS INSIDE EACH COLUMNS
+        * CREATES BUTTONS OUTSIDE THE ELEVATORS(WHICH HANDLE THE FIRST REQUEST FOR AN ELEVATOR)
+        * FIND THE NEAREST ELEVATOR( MATH FUNCTION : FLOOR RQUESTED FROM - ACTUAL POSITION => WHICH ON IS CLOSEST TO 0)
+        * SEND THE ELEVATOR TO IT DESTINATION
+        
+2. ELEVATOR
 
-You can also get more details about each test by adding the `-v` flag: 
+        * HANDLE ELEVATOR ATTRIBUTES
+        * CREATE BUTTONS INSIDE THE ELEVATORS
+        * WAIT FOR AN DESTINATION FLOOR
+        * MOVE THE ELEVATOR THROUGH THE QUEUE LIST UNTIL THERE IS NO MORE REQUEST
+        * OPERATES THE DOOR OF THE ELEVATOR
 
-`pytest -v` 
+3. CALLBUTTON
 
-which should give something like: 
+        * HANDLE THE OUTSIDE BUTTONS ATTRIBUTES
 
-![Screenshot from 2021-06-15 13-13-33](https://user-images.githubusercontent.com/28630658/122095759-c74a4f80-cddb-11eb-999d-dfe35dbe7d18.png)
+4. FLOORRQUESTBUTTON:
 
-The test file can be left in your final project but no scenarios should be present in your code. The grader will run tests similar to the ones provided.
+        * HANDLE THE INSIDE BUTTONS ATTRIBUTES
 
-Of course, make sure to edit this Readme file to describe your own project!
+5. DOORS:
+
+        * HANDLE THE DOORS ATTRIBUTES
+
+
+============== THE LOGIC OF THIS PYTHON SOURCE CODE ============== 
+
+
+We receive a request for an elevator, the elevator move to the request floor.
+Open its door and wait for the Client to enter his destination floor.
+We add this destination floor to the queue list(List of the floors to be done).
+
+How Do we select an elevator: 
+    * We check it's status first
+    * If idle, we add it to the list of possible elevator
+    * If moving, but it's moving to the same direction as the requested floor
+    * We check the gap betweeen the request floor and the current floor of the elevator
+    * Which elevator's gap is closest to the 0, it's the one that is chosen.
+    
--- residential_controller.py
+++ residential_controller.py
@@ -1,18 +1,148 @@
+import time
+callButtonID = 1
+
+# ******     THE OBJECT FUNCTION THAT CREATES THE COLUMNS     ******
 class Column:
     def __init__(self, _id, _amountOfFloors, _amountOfElevators):
+        self.ID = _id
+        self.amountOfFloors = _amountOfFloors
+        self.status = "idle"
+        self.amountOfElevators = _amountOfElevators
+        self.elevatorList = []
+        self.callButtonList = []
+        for i in range(self.amountOfElevators):
+            self.elevatorList.append(Elevator(i + 1,self.amountOfFloors))
+        self.createCallButtons()
+    # FUNCTION WITH A FOR LOOP TO CREATE 18 BUTTONS
+    def createCallButtons(self):
+        global callButtonID
+        buttonFloor = 1
+        for i in range(self.amountOfFloors):
+            if buttonFloor < self.amountOfFloors:
+                self.callButtonList.append(CallButton(i,buttonFloor,'up'))
+                callButtonID += 1
+            if buttonFloor > 1:
 
+                 self.callButtonList.append(CallButton(i,buttonFloor,'down'))
+                 callButtonID += 1
+            buttonFloor = buttonFloor + 1
+        
+    # FUNCTION THAT FINDS THE NEAREST ELEVATOR AND RETURN IT'S ID
+    def findElevator(self,floor,direction):
+        selectableElevator = []
+        numbers = []
+
+        for i in range(len(self.elevatorList)):
+                if self.elevatorList[i].status == "idle":
+                    selectableElevator.append(self.elevatorList[i].ID)
+                elif self.elevatorList[i].direction == 'up' and direction == 'up':
+
+                    selectableElevator.append(self.elevatorList[i].ID)
+                elif self.elevatorList[i].direction == 'down' and direction == 'down':
+
+                    selectableElevator.append(self.elevatorList[i].ID)
+        # LIST OF THE SELECATABLE ELEVATORS THAT COULD BE USED
+        # MATH FUNCTION THAT RETURNS FIND THE CLOSEST ELEVATOR AND RETURN ITS ID
+        for i in range(len(selectableElevator)):
+            numbers.append(floor - self.elevatorList[i].currentFloor)
+     
+        return numbers.index(numbers[min(range(len(numbers)), key = lambda i: abs(numbers[i]-0))])
+
+    # FUNCTION THAT HANDLE THE ELEVATOR REQUEST: FIND THE BEST ELEVATOR, ADD THE FLOOR TO REQUEST LIST, CALL TO MOVE AND OPERATE DOORS
+    
+    def requestElevator(self,floor,direction):
+        elevator = self.findElevator(floor,direction)
+        print(elevator)
+        self.elevatorList[elevator].floorRequestList.append(floor)
+        self.elevatorList[elevator].move()
+        self.elevatorList[elevator].operateDoors
+
+        return self.elevatorList[elevator]
+
+
+# ******     THE FUNCTION THAT CREATES THE ELEVATORS AND HANDLES THEMS     ******
 
 class Elevator:
     def __init__(self, _id, _amountOfFloors):
+        self.ID = _id
+        self.amountOfFloors = _amountOfFloors
+        self.status = "idle"
+        self.direction = ""
+        self.door = Door(0)
+        self.currentFloor = 1
+        self.floorRequestButtonList = []
+        self.floorRequestList = []
+        # FUNCTION THAT CREATES THE BUTTON INSIDE THE ELEVATOR
+      
+        for i in range(self.amountOfFloors):
+            self.floorRequestButtonList.append(FloorRequestButton(i,i))
+
+    
+    
+    # FUNCTION THAT GETS THE DESTINATION FLOOR AND ADD IT TO THE QUEUE
+    
+    def requestFloor(self,floor):
+        self.floorRequestList.append(floor)
+        self.move()
+
+    # FUNCTION THAT MAKES THE SELECTED ELEVATOR MOVE TO THE DESTINATION
+    def move(self):
+        while self.floorRequestList:
+            destination = self.floorRequestList[0]
+            self.status = "MOVING"
+
+            if self.currentFloor < destination:
+                self.direction = 'up'
+                self.sortFloorList()
+                while self.currentFloor < destination:
+                    self.currentFloor = self.currentFloor + 1
+            elif    self.currentFloor > destination:
+                self.direction = 'down'
+                self.sortFloorList()
+                while self.currentFloor > destination:
+                    self.currentFloor = self.currentFloor - 1
+            self.status = "STOPPED"
+            self.floorRequestList.pop(0)
+
+        self.status = 'idle'
 
 
+    #  FUNCTION THAT SORT THE QUEUE LIST IN ORDER OF THE DIRECTION
+     
+    def sortFloorList(self):
+        if self.direction == 'up':
+            self.floorRequestList.sort()
+        elif self.direction == 'down':
+            self.floorRequestList.sort(reverse = True)
+    # FUNCTION THAT OPERATES THE DOOR : CLOSE OR OPEN IT
+    def operateDoors(self):
+
+        if(self.door.status == 'OPEND'):
+            time.sleep(5)
+            self.door.status == 'CLOSED'
+        elif self.door.status== 'CLOSED' :
+            time.sleep(5)
+            self.door.status == 'OPEN'
+
+
+
+
+#          ******     THE FUNCTION THAT CREATES THE EXTERIOR BUTTONS(OUTSIDE ELEVATORS)     ******
 class CallButton:
     def __init__(self, _id, _floor, _direction):
+        self.ID = _id
+        self.floor = _floor
+        self.direction = _direction
+        self.status = 'on'
 
-
+#          ******     THE FUNCTION THAT CREATES THE INTERIOR BUTTONS(INSIDE ELEVATORS)   ******
 class FloorRequestButton:
     def __init__(self, _id, _floor):
-
-
+        self.ID = _id
+        self.floor = _floor
+        self.status = 'on'
+#          ******     THE FUNCTION THAT CREATES THE DOORS     ******
 class Door:
     def __init__(self, _id):
+        self.ID = _id
+        self.status = 'closed'
